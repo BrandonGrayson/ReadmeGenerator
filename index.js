0dfc52b625fe0ead9f2c6bafe71dd467a065dba3
Brandon Grayson <brandongrayson@Brandons-Air.fios-router.home>
2020-12-17 21:37:21 -0500

    input commands functional

--- index.js
+++ index.js
@@ -13,7 +13,34 @@ const questions = [
         name: 'description',
         message: 'How would you best describe your project?'
     },
-    
+    // ask the user for installation instructions
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Are there any installation instructions users should be aware of?'
+    },
+    // Access any Usage Information
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Is there any usage information?'
+    },
+    // Contribution Guidelines
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'What are the contribution guidelines for this project?'
+    },
+    // test instruction
+    {
+        type: 'input',
+        name: 'test',
+        message: 'what are the test instructions?'
+    },
+    // list of certifications for project
+    {
+        type: 'list'
+    }
 ];
 
 // function to write README file
